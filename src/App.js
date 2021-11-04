37ca8adcfd6a1e4d0dcebceb91806ec7a1f5b769
Wolz <wolz.dev@gmail.com>
2021-11-04 14:25:24 -0300

    termino la clase 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import ListOfGifs from './components/ListOfGifs'
 
-const GIFS = [
-  'https://media0.giphy.com/media/iMYbnEuQ36GezAw9nF/200w.webp',
-  'https://media0.giphy.com/media/4GIvR6cmYs8jC/200w.webp',
-  'https://media2.giphy.com/media/3oKIPdL94CCIbgVcRy/giphy.gif?cid=ecf05e4743827591e2f7207a8d86122421ab9908828beb1d&rid=giphy.gif&ct=g'
-]
-  ;
+import { Route, Link } from "wouter"
 
 function App() {
-  const [gifs, setGifs] = useState(GIFS)
-
   return (
     <div className="App">
       <section className="App-content">
-        {
-          gifs.map(singleGif => <img src={singleGif} />)
-        }
-
+        <h1>APP</h1>
+        <Link to="/gif/panda">Gifs de Pandas</Link>
+        <Link to="/gif/chile">Gifs de Chile</Link>
+        <Link to="/gif/ecuador">Gifs de Ecuador</Link>
+        <Route
+          component={ListOfGifs}
+          path="/gif/:keyword" />
       </section>
     </div>
   );
---
+++ src/components/ListOfGifs.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react"
+import getGits from "../services/getGifs"
+import Gif from "./Gif"
+
+
+export default function ListOfGifs({ params }) {
+    const { keyword } = params
+    const [loading, setLoading] = useState(false)
+
+    const [gifs, setGifs] = useState([])
+
+    useEffect(function () {
+        setLoading(true)
+        getGits({ keyword })
+            .then(gifs => {
+                setGifs(gifs)
+                setLoading(false)
+            })
+    }, [keyword])
+
+    if (loading) return <i>❤️‍🔥</i>
+
+    return <div>
+        {
+            gifs.map(({ id, title, url }) =>
+                <Gif
+                    id={id}
+                    key={id}
+                    title={title}
+                    url={url}
+                />
+            )
+        }
+    </div>
+}
